6154488f974f647c2a8251e2f3c99801b4dc6868
Mark Gobriel <markgobriel@gmail.com>
2024-10-08 17:43:11 -0400

    test commit

--- app/src/main/java/com/example/kutenda/MainActivity.kt
+++ app/src/main/java/com/example/kutenda/MainActivity.kt
@@ -51,6 +51,7 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    // test comment
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
